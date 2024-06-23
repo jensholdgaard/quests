5660e4e9243d9579002a8a237e7508e37500cbba
E Spause <espause0703@gmail.com>
2024-06-22 20:40:26 -0400

    Shady gobbo no longer depops.

--- warslikswood/a_shady_goblin.lua
+++ warslikswood/a_shady_goblin.lua
@@ -18,7 +18,7 @@ function event_trade(e)
 	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 6475, item2 = 6471})) then -- forged note & Drogan Warlord's Insignia
 		e.self:Emote("gapes at the note in puzzlement, obviously a bit concerned by it. The courier looks at the note for more than a minute, then looks at you, and back at the note. 'Errm... welp, dunno what to make of the note. If I know the right of it, Skargus finally got him some girly to do his writin' for him. Ooooh I bet she a tasty one, graarr. If I was warlord I know I'd have me a girly too. Gah, take this report and get outta my sight.");
 		e.other:QuestReward(e.self,0,0,0,0,6474); -- 6474  Report to Skargus
-		eq.depop_with_timer();
+		--eq.depop_with_timer();
 	end
 	item_lib.return_items(e.self, e.other, e.trade)
 end
