c0c8e6eea25a471ce77b6ac8484ffc9f4524d76b
E Spause <espause0703@gmail.com>
2024-06-23 22:16:09 -0400

    Add agent of druzzil explaining where your dragon bodies are.

---
+++ burningwood/Agent_of_Druzzil.lua
@@ -0,0 +1,15 @@
+-- Converted to .lua by Speedz
+
+function event_say(e)
+	if(e.message:findi("Hail")) then
+		e.self:Say("Greetings " .. e.other:GetCleanName() .. ". If you're looking for your bodies, you may wish to look near Chardok. There seems to be a temporal flux preventing the distribution of corpses to this specific area.");
+	elseif(e.message:findi("bod")) then
+		e.self:Say("Oh, you can't find your body? Allow me to make one.");
+		e.self:CastSpell(982,e.other:GetID(),0,1); -- Spell: Bind Affinity
+	end
+end
+
+function event_trade(e)
+	local item_lib = require("items");
+	item_lib.return_items(e.self, e.other, e.trade)
+end
