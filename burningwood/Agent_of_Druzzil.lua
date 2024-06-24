199f974674d3f8b6db97962191ad3f0a718eba93
E Spause <espause0703@gmail.com>
2024-06-23 22:17:18 -0400

    Adjusted 'gotcha' text.

--- burningwood/Agent_of_Druzzil.lua
+++ burningwood/Agent_of_Druzzil.lua
@@ -2,7 +2,7 @@
 
 function event_say(e)
 	if(e.message:findi("Hail")) then
-		e.self:Say("Greetings " .. e.other:GetCleanName() .. ". If you're looking for your bodies, you may wish to look near Chardok. There seems to be a temporal flux preventing the distribution of corpses to this specific area.");
+		e.self:Say("Greetings " .. e.other:GetCleanName() .. ". If you're looking for your bodies from your ethereal fight with Talendor, you may wish to look near Chardok. There seems to be a temporal flux preventing the distribution of corpses to this specific area. If you have no [bodies], I could maybe assist with that. Just ask.");
 	elseif(e.message:findi("bod")) then
 		e.self:Say("Oh, you can't find your body? Allow me to make one.");
 		e.self:CastSpell(982,e.other:GetID(),0,1); -- Spell: Bind Affinity
