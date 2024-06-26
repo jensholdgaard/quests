31c8660ae910518ab8376a38ba1dba810cd86ec8
E Spause <espause0703@gmail.com>
2024-06-26 12:22:02 -0400

    Remove Nagafen/Vox Banish.

--- permafrost/Lady_Vox.lua
+++ permafrost/Lady_Vox.lua
@@ -1,5 +1,3 @@
--- Attempt to limit players level 53 and over from engaging nagafen and vox.
--- Banished to Permafrost.
 local SpawnX = 0;
 local SpawnY = 0;
 local SpawnZ = 0;
@@ -10,16 +8,6 @@ function event_spawn(e)
 	SpawnY = e.self:GetY();
 	SpawnZ = e.self:GetZ();
 	SpawnH = e.self:GetHeading();
-	local range = 210;
-	local range2 = 98;
-	eq.set_proximity(SpawnX - range, SpawnX + range, SpawnY - range2, SpawnY + range, SpawnZ - 20, SpawnZ + 50);
-end
-
-function event_enter(e)
-	if(e.other:GetLevel() > 52 and e.other:Admin() < 80) then
-		e.other:Message(4,"I will not fight you, but I shall banish you!");
-		e.other:MovePC(30,-7024,2020,-60.7,0); -- Zone: burningwood 
-	end
 end
 
 function event_combat(e)
@@ -35,26 +23,6 @@ function event_timer(e)
 	if(e.timer == "1") then
 		if(e.self:GetX() < -431 or e.self:GetX() > -85 or e.self:GetY() < 770 or e.self:GetY() > 1090 or e.self:GetZ()  < -50) then
 			e.self:GMMove(SpawnX,SpawnY,SpawnZ,SpawnH);
-		elseif(e.self:CountHateList() > 0) then
-			e.self:ForeachHateList(
-				function(ent, hate, damage, frenzy)
-					if(ent:IsClient()) then
-						ent:CastToClient():Message(4,"I will not fight you, but I shall banish you!");
-						if(ent:CastToClient():GetBindZoneID() == 73) then
-							ent:CastToClient():SetBindPoint(30,-7024,2020,-60);
-						end
-						ent:CastToClient():MovePC(30,-7024,2020,-60.7,0);
-					end
-				end,
-				function(ent, hate, damage, frenzy)
-					if(ent:IsClient()) then
-						if(ent:CastToClient():GetLevel() > 52) then
-							return true;
-						end
-					end
-					return false;
-				end
-			);
 		end
 	end
 end
--- soldungb/Lord_Nagafen.lua
+++ soldungb/Lord_Nagafen.lua
@@ -1,5 +1,3 @@
--- Attempt to limit players level 53 and over from engaging nagafen and vox.
--- Banished to Lavastorm.
 local SpawnX = 0;
 local SpawnY = 0;
 local SpawnZ = 0;
@@ -10,15 +8,6 @@ function event_spawn(e)
 	SpawnY = e.self:GetY();
 	SpawnZ = e.self:GetZ();
 	SpawnH = e.self:GetHeading();
-	local range = 230;
-	eq.set_proximity(SpawnX - range, SpawnX + range, SpawnY - range, SpawnY + range);
-end
-
-function event_enter(e)
-	if(e.other:GetLevel() > 52 and e.other:Admin() < 80) then
-		e.other:Message(4,"I will not fight you, but I shall banish you!");
-		e.other:MovePC(27,534,913,55,0); -- Zone: bothunder 
-	end
 end
 
 function event_combat(e)
@@ -34,26 +23,6 @@ function event_timer(e)
 	if(e.timer == "1") then
 		if(e.self:GetX() < -1000 or e.self:GetX() > -650 or e.self:GetY() < -1500 or e.self:GetY() > -1170) then
 			e.self:GMMove(SpawnX,SpawnY,SpawnZ,SpawnH);
-		elseif(e.self:CountHateList() > 0) then
-			e.self:ForeachHateList(
-				function(ent, hate, damage, frenzy)
-					if(ent:IsClient()) then
-						ent:CastToClient():Message(4,"I will not fight you, but I shall banish you!");
-						if(ent:CastToClient():GetBindZoneID() == 32) then
-							ent:CastToClient():SetBindPoint(27,534,913,55);
-						end
-						ent:CastToClient():MovePC(27,534,913,55,0);
-					end
-				end,
-				function(ent, hate, damage, frenzy)
-					if(ent:IsClient()) then
-						if(ent:CastToClient():GetLevel() > 52) then
-							return true;
-						end
-					end
-					return false;
-				end
-			);
 		end
 	end
 end
