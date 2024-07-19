5227d99fd1a7d911b07a351216951c9494b90379
E Spause <espause0703@gmail.com>
2024-07-19 13:27:15 -0400

    boat?

--- timorous/Island_Shuttle.lua
+++ timorous/Island_Shuttle.lua
@@ -1,4 +1,11 @@
-function event_signal(e) 		
+function event_signal(e) 	
+
+	local zoneGuildID = eq.get_zone_guild_id();
+
+	if(zoneGuildID == 4294967295) then
+		return false;
+	end
+
 	if(e.signal == 1) then 		
  		eq.start(3); 		
  	elseif(e.signal == 2) then 		
