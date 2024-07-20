b71cf65514a15392e97881a5899455f28c80ecde
ryhoneyman <ryhoneyman@gmail.com>
2024-07-20 12:16:51 +0000

    prevent boats in instances from spawning

--- timorous/Captains_Skiff.lua
+++ timorous/Captains_Skiff.lua
@@ -1,4 +1,11 @@
-function event_signal(e) 		
+function event_spawn(e)
+	if (eq.get_zone_guild_id() ~= 0xFFFFFFFF) then
+		eq.debug("We are in an instance, ignoring event_spawn for " .. e.self:GetName());
+		eq.depop();
+    end
+end
+
+function event_signal(e) 
 	if(e.signal == 1) then 		
  		eq.start(4);
 	end
