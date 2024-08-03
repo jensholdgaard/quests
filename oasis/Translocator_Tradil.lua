45f1a32e49010b92a8f0decc9b494c0c4c94b997
ryhoneyman <ryhoneyman@gmail.com>
2024-08-03 18:37:16 +0000

    Added shuttle dock translocator quests

---
+++ oasis/Translocator_Tradil.lua
@@ -0,0 +1,8 @@
+function event_say(e)
+	if(e.message:findi("Hail")) then
+		e.self:Say("Hello there, " .. e.other:GetCleanName() .. ". There seem to be some strange problems with the boats in this area. The Academy of Arcane Sciences has sent a small team of us to investigate them. In the meantime, I can transport you to my companion there if you wish to [travel to Timorous Deep].");
+	elseif(e.message:findi("timorous deep")) then
+		e.self:CastSpell(2291,e.other:GetID()); -- Spell: Portal to Timorous (ogre camp)
+		e.self:Say("Off you go!");
+	end
+end
