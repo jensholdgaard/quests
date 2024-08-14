ce587b0166d085ab6355595d52c6995b4fb198a1
fuzzlecutter <164541008+fuzzlecutter@users.noreply.github.com>
2024-08-14 21:14:42 +0000

    Add self found mind melt quest

--- rathemtn/Hasten_Bootstrutter.lua
+++ rathemtn/Hasten_Bootstrutter.lua
@@ -4,6 +4,7 @@ end
 
 function event_say(e)
 	local rebreathers_lib = require("self_found_rebreathers");
+	local mind_melt_lib = require("self_found_mind_melt");
 	
 	if(e.message:findi("hail")) then
 		e.self:Say("Stopped I have done. Greet you I will. What business do we have? Hmm? Speak up!!");
@@ -16,17 +17,20 @@ function event_say(e)
 	elseif(e.message:findi("ring of the ancient")) then
 		e.self:Say("Ancient cyclopes do march the land.  Long have they lived and crafted the rings of the ancients.  I must have one and you shall go and fetch one for me.  Seek the lands of the oldest cyclops clans.  Seek the plains. seek the island in tears and search the dunes for there is one who is last.  His clan was blown from the sands.");
 	elseif(rebreathers_lib.check_dialog(e.self, e.other, e.message)) then
+	elseif(mind_melt_lib.check_dialog(e.self, e.other, e.message)) then
 	end
 end
 
 function event_trade(e)
 	local item_lib = require("items");
 	local rebreathers_lib = require("self_found_rebreathers");
+	local mind_melt_lib = require("self_found_mind_melt");
 	
 	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 12268, item2 = 7100, platinum = 325})) then
 		e.self:Say("The time to trade has come!! I am now rich and you are now fast. Take the Journeyman Boots and run like the wind.");
 		e.other:QuestReward(e.self,0,0,0,0,2300,1250);
 	elseif(rebreathers_lib.check_turn_in(item_lib, e.self, e.other, e.trade)) then
+	elseif(mind_melt_lib.check_turn_in(item_lib, e.self, e.other, e.trade)) then
 	end
 	item_lib.return_items(e.self, e.other, e.trade)
 end
