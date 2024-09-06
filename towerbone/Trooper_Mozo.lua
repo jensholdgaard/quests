b1e71fded476d3566e97d899f763b5f61001f955
ikiris <4852950+ikiris@users.noreply.github.com>
2024-09-06 12:39:15 -0700

    Update Trooper_Mozo.lua bone chip quest to be multi passable
    Not completely sure if this is right in terms of item return.

--- towerbone/Trooper_Mozo.lua
+++ towerbone/Trooper_Mozo.lua
@@ -10,18 +10,22 @@ end
 function event_trade(e)
 	local item_lib = require("items");
 	local text = "I don't see four bone chips.";		
-	
-	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 13073, item2 = 13073, item3 = 13073, item4 = 13073},1,text)) then --Handin: 4x Bone chips
-		e.self:Emote("gasp's in astonishment.");
-		e.self:Say("Wha...? This!! But... I... Why you... I oughta.. All right!! A bet is a bet. Here you go. A few silver piece's for your achievement. What?!! You thought I would give you ten? Fat chance! Now get lost or I will haul you in for impersonating a froglok.");
-		e.other:Faction(e.self,441,1); -- +Legion of Cabilis
-		e.other:Faction(e.self,440,1);  -- +Cabilis Residents
-		e.other:Faction(e.self,445,1); -- +Scaled Mystics
-		e.other:Faction(e.self,442,1);  -- +Crusaders of Greenmist
-		e.other:Faction(e.self,444,1); -- +Swifttails
-		-- Confirmed Live Experience
-		e.other:QuestReward(e.self,0,math.random(10),0,0,0,250);
-	end
+
+	local bone_chips = item_lib.count_handed_item(e.self, e.trade, {13073}, 4);
+	if(bone_chips > 0) then
+		repeat
+			e.self:Emote("gasp's in astonishment.");
+			e.self:Say("Wha...? This!! But... I... Why you... I oughta.. All right!! A bet is a bet. Here you go. A few silver piece's for your achievement. What?!! You thought I would give you ten? Fat chance! Now get lost or I will haul you in for impersonating a froglok.");
+			e.other:Faction(e.self,441,1); -- +Legion of Cabilis
+			e.other:Faction(e.self,440,1);  -- +Cabilis Residents
+			e.other:Faction(e.self,445,1); -- +Scaled Mystics
+			e.other:Faction(e.self,442,1);  -- +Crusaders of Greenmist
+			e.other:Faction(e.self,444,1); -- +Swifttails
+			-- Confirmed Live Experience
+			e.other:QuestReward(e.self,0,math.random(10),0,0,0,250);
+			bone_chips = bone_chips - 1
+		until bone_chips == 0
+	end	
 	item_lib.return_items(e.self, e.other, e.trade)
 end
 
