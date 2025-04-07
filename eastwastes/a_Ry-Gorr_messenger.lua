4f3dea8366b059d9ec06145203ab7257c0a76cad
jududdar <jududdar@users.noreply.github.com>
2025-04-07 10:56:50 -0400

    Change respawn timer from 4 hours to 5 minutes

--- eastwastes/a_Ry-Gorr_messenger.lua
+++ eastwastes/a_Ry-Gorr_messenger.lua
@@ -8,5 +8,5 @@ end
 function event_death(e)
 	local sp = e.self:GetSpawnPointID();
 	local spawn = eq.get_entity_list():GetSpawnByID(sp);
-	spawn:SetRespawnTimer(14400);
+	spawn:SetRespawnTimer(300);
 end
