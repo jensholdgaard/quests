8fc02e443aeea45e9ec1556e3a3d067d60df7f99
Edalyn <espause0703@gmail.com>
2025-05-26 21:28:05 -0400

    Update Avatar_of_Below.lua

--- iceclad2/Avatar_of_Below.lua
+++ iceclad2/Avatar_of_Below.lua
@@ -1,7 +1,7 @@
 function event_trade(e)
 	local item_lib = require("items");
 	
-	if(item_lib.check_turn_in(e.self, e.trade, {item1 = 1199}, 0) or item_lib.check_turn_in(e.self, e.trade, {item1 = 8895}, 0)) then -- Runed Coldain Prayer Shawl (mark A or mark B)
+	if(item_lib.check_turn_in_nomq(e.self, e.trade, {item1 = 1199}, 0) or item_lib.check_turn_in_nomq(e.self, e.trade, {item1 = 8895}, 0)) then -- Runed Coldain Prayer Shawl (mark A or mark B)
 		e.self:Emote("holds the shawl up close to one eye to look at the rune sewn into it. He smiles to himself and then drops it to the ground and places one hand on it. A bright blue glow starts to emanate in the snow, then there is a sudden flash! The Avatar has dissipated. All that is left behind is the Shawl lying in the snow, shimmering with a new power. " .. e.other:GetCleanName() .. " slowly bends down to pick it up, and is infused with a blessing from Brell.");
 		e.other:QuestReward(e.self,0,0,0,0,1200,100000); -- Blessed Coldain Prayer Shawl
 		eq.get_entity_list():GetMobByNpcTypeID(110019):Say("I must go tell the Dain of these events immediately!");
