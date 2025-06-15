02d193da17cbbc8e71606901633d662b4e47fe75
Edalyn <espause0703@gmail.com>
2025-06-14 20:17:45 -0400

    Update Translocator_Tradil.lua

--- oasis/Translocator_Tradil.lua
+++ oasis/Translocator_Tradil.lua
@@ -4,5 +4,9 @@ function event_say(e)
 	elseif(e.message:findi("timorous deep")) then
 		e.self:CastSpell(2291,e.other:GetID()); -- Spell: Portal to Timorous (ogre camp)
 		e.self:Say("Off you go!");
+	elseif(e.message:findi("I am cis")) then
+		e.self:Say("Ah. Then you might be needing my brother, Cislocator instead. He should be close by!");
+	elseif(e.message:findi("I am trans")) then
+		e.self:Say("Ah. Then you came to the right place! I can trans port you to my companion there if you wish to [travel to Timorous Deep].");
 	end
 end
